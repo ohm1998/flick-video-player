04eb96ddc46030ba8a2585fd82cbae69095c0a67
ohm trivedi <trivedi@ohms-MacBook-Air.local>
2021-07-27 16:54:27 +0530

    close button added

--- lib/src/controls/flick_portrait_controls.dart
+++ lib/src/controls/flick_portrait_controls.dart
@@ -26,80 +26,103 @@ class FlickPortraitControls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: <Widget>[
-        Positioned.fill(
-          child: FlickShowControlsAction(
-            child: FlickSeekVideoAction(
-              child: Center(
-                child: FlickVideoBuffer(
-                  child: FlickAutoHideChild(
-                    showIfVideoNotInitialized: false,
-                    child: FlickPlayToggle(
-                      size: 30,
-                      color: Colors.black,
-                      padding: EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: Colors.white70,
-                        borderRadius: BorderRadius.circular(40),
+      children: [
+        Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: FlickShowControlsAction(
+                child: FlickSeekVideoAction(
+                  child: Center(
+                    child: FlickVideoBuffer(
+                      child: FlickAutoHideChild(
+                        showIfVideoNotInitialized: false,
+                        child: FlickPlayToggle(
+                          size: 30,
+                          color: Colors.black,
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.white70,
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ),
-        Positioned.fill(
-          child: FlickAutoHideChild(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  FlickVideoProgressBar(
-                    flickProgressBarSettings: progressBarSettings,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Positioned.fill(
+              child: FlickAutoHideChild(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
-                      FlickPlayToggle(
-                        size: iconSize,
-                      ),
-                      SizedBox(
-                        width: iconSize / 2,
-                      ),
-                      FlickSoundToggle(
-                        size: iconSize,
-                      ),
-                      SizedBox(
-                        width: iconSize / 2,
+                      FlickVideoProgressBar(
+                        flickProgressBarSettings: progressBarSettings,
                       ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          FlickCurrentPosition(
-                            fontSize: fontSize,
+                          FlickPlayToggle(
+                            size: iconSize,
                           ),
-                          FlickAutoHideChild(
-                            child: Text(
-                              ' / ',
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: fontSize),
-                            ),
+                          SizedBox(
+                            width: iconSize / 2,
                           ),
-                          FlickTotalDuration(
-                            fontSize: fontSize,
+                          FlickSoundToggle(
+                            size: iconSize,
                           ),
+                          SizedBox(
+                            width: iconSize / 2,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              FlickCurrentPosition(
+                                fontSize: fontSize,
+                              ),
+                              FlickAutoHideChild(
+                                child: Text(
+                                  ' / ',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: fontSize),
+                                ),
+                              ),
+                              FlickTotalDuration(
+                                fontSize: fontSize,
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Container(),
+                          ),
+                          // FlickFullScreenToggle(
+                          //   size: iconSize,
+                          // ),
                         ],
                       ),
-                      Expanded(
-                        child: Container(),
-                      ),
-                      // FlickFullScreenToggle(
-                      //   size: iconSize,
-                      // ),
                     ],
                   ),
-                ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: Positioned.fill(
+              child: IconButton(
+                icon: Icon(
+                  Icons.close,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                onPressed: () {
+                  print("navigator context pop called cross button");
+                  Navigator.pop(context);
+                },
               ),
             ),
           ),
